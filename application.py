7e5bc201180538bdd18546500d20a75743c55a61
Venkat Sai Subash Panchakarla <88431249+venkatsubash2003@users.noreply.github.com>
2024-09-11 16:16:40 +0530

    Update application.py

--- application.py
+++ application.py
@@ -8,7 +8,7 @@ from src.pipeline.predict_pipeline import CustomData,PredictPipeline
 application=Flask(__name__)
 
 app=application
-
+# Added a new comment
 ## Route for a home page
 
 @app.route('/')
