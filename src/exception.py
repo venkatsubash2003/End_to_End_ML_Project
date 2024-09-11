831ce9758d0e7ac69be16ad51a76a8fdfd811049
venkatsubash2003 <venkatsaisubash.2003@gmail.com>
2024-09-11 14:25:48 +0530

    some commits

--- src/exception.py
+++ src/exception.py
@@ -1,5 +1,5 @@
 import sys
-from logger import logging
+from src.logger import logging
 
 def error_message_detail(error,error_detail:sys):
     _,_,exc_tb=error_detail.exc_info()
@@ -7,6 +7,7 @@ def error_message_detail(error,error_detail:sys):
     error_message = "Error occured in Python script name [{0}] line number [{1}] error format [{2}]".format(
         file_name,exc_tb.tb_lineno,str(error)
     )
+    return error_message
 
 class CustomException(Exception):
     def __init__(self,error_message,error_detail:sys):
--- src/logger.py
+++ src/logger.py
@@ -1,7 +1,7 @@
 import logging
 import os
 from datetime import datetime
-from exception import CustomException
+from src.exception import CustomException
 
 LOG_FILE = f"{datetime.now().strftime('%m_%d_%Y_%H_%M_%S')}.log"
 logs_path = os.path.join(os.getcwd(),"logs",LOG_FILE)
